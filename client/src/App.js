ffc366ab1e5542b82b3a0e609fe0460915198c24
anhkhoaoki <132700613+anhkhoaoki@users.noreply.github.com>
2024-12-03 16:05:41 +0700

    creat historypage printpage purchasepage for student

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import HistoryPage from './pages/HistoryPage.tsx';
+import PrintPage from './pages/PrintPage.tsx';
+import PurchasePage from './pages/PurchasePage.tsx';
+
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/print" element={<PrintPage/>}/>
+          <Route path="/purchase" element={<PurchasePage/>} />
+          <Route path="/history" element={<HistoryPage/>} />
         </Routes>
       </Router>
     </div>
