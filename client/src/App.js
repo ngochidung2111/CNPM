2b8aec8a0e54f32d68825793186c7278f5a551bb
Trần Nguyễn Anh Khoa <trannguyenanhkhoa0104@gmail.com>
2024-12-03 23:52:34 +0700

    cơ bản homepage, login, decentralization,  signup, infostudent: chỉ là ui chưa logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx';
-
+import HomePage from './pages/HomePage.tsx';
+import Decontralization from './pages/Decontralization.tsx';
+import Login from './pages/Login.tsx';
+import SignUp from './pages/Signup.tsx';
+import InfoUser from './pages/InfoUser.tsx';
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="phanquyen" element={<Decontralization />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/info" element={<InfoUser />} />
         </Routes>
       </Router>
     </div>
