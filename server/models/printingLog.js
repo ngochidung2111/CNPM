da26929850a3febdf3535996132572d7c8515c67
tacsquang <tac.tranquang@hcmut.edu.vn>
2024-12-05 12:04:27 +0700

    update models printingLog

--- server/models/printingLog.js
+++ server/models/printingLog.js
@@ -4,8 +4,8 @@ const printingLogSchema = new mongoose.Schema({
     studentId:  { type: String, ref: 'Student', required: true }, // Mã số sinh viên
     printerId:  { type: String, ref: 'Printer', required: true }, // Mã máy in
     fileName:   { type: String, required: true },
-    startTime:  { type: Date, required: true },
-    endTime:    { type: Date, required: true },
+    startTime:  { type: Date, default: Date.now },
+    endTime:    { type: Date },
     paperUsage: {                                 // Lượng giấy in
         A4: { type: Number, default: 0 },
         A3: { type: Number, default: 0 },
@@ -16,6 +16,8 @@ const printingLogSchema = new mongoose.Schema({
         copies:   { type: Number, required: true },
         isDoubleSided: { type: Boolean, default: false },
     },
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed', 'Canceled'], default: 'Pending' },
+
 }, { timestamps: true });
 
 // Thêm index
@@ -24,5 +26,6 @@ printingLogSchema.index({ printerId: 1 });
 printingLogSchema.index({ startTime: -1 });        // Thời gian mới nhất trước
 printingLogSchema.index({ endTime: -1 });
 printingLogSchema.index({ studentId: 1, printerId: 1 }); // Chỉ mục kết hợp nếu tìm theo sinh viên và máy in thường xuyên
+printingLogSchema.index({ status: 1 });
 
 module.exports = mongoose.model('PrintingLog', printingLogSchema);
