47395c845d1d9e2ebd80992173a49b72c3194ef7
Trần Nguyễn Anh Khoa <trannguyenanhkhoa0104@gmail.com>
2024-12-13 09:37:30 +0700

    logic page login/info

--- client/src/pages/InfoUser.tsx
+++ client/src/pages/InfoUser.tsx
@@ -3,38 +3,142 @@ import Button from '../components/InfoUserItems/Button.tsx'
 import InfoFrame from '../components/InfoUserItems/InfoFrame.tsx'
 import Header from '../components/header/header.tsx'
 import Sidebar from './../components/sidebars/Sidebar.tsx';
+import { data } from 'react-router-dom';
 interface Info {
   label: string,
   value: string
 }
+
+
 const InfoUser: React.FC = () => {
+  
+  const [info, setInfo] = useState<Info[]>([]);
+
+  useEffect(() => {
+    
+
+    const id = 2211651 // how to get it???????????
+    let fetchedData;
+    
+    const fetchData = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken')
+        if(accessToken) {
+          const response = await fetch(`http://localhost:5000/api/students/${id}`, {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          if(response.status === 200) {
+            console.log('api get student success');
+            
+            fetchedData = await response.json();
+            const fetchedInfo = [
+              {
+                label: 'Họ và tên',
+                value: fetchedData.name
+              },
+              {
+                label: 'Mã số sinh viên',
+                value: fetchedData._id
+              },
+              {
+                label: 'Khoa',
+                value: 'Khoa hoc va Ky thuat May tinh'
+              },
+              {
+                label: 'Ngày tháng năm sinh',
+                value: '10/6/2004'
+              },
+              {
+                label: 'Phái',
+                value: 'Nam'
+              },
+              {
+                label: 'Địa chỉ',
+                value: 'Binh Duong'
+              },
+              {
+                label: 'Email',
+                value: fetchedData.email
+              },
+              {
+                label: 'Số điện thoại',
+                value: '0909090909'
+              }]
+              console.log('fetchedInfo: ', fetchedInfo);
+              
+            setInfo(fetchedInfo);
+            console.log();
+            
+          }
+        }
+        else {
+          console.error('Access token not found'); 
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();     
+
+  }, []);
+
+
+
+
   const menuItems = [
     { title: "In tài liệu", link: "/print" },
     { title: "Mua thêm trang in", link: "/mua-them-trang-in" },
     { title: "Xem lịch sử in", link: "/lich-su-in" },
   ];
-    const [info, setInfo] = useState([
-        { label: 'Họ và tên', value: 'Nguyễn Văn A' },
-        { label: 'Mã số sinh viên', value: '2222222' },
-        { label: 'Khoa', value:'Khoa học và Kỹ thuật Máy tính' },
-        { label: 'Ngày tháng năm sinh', value: '01/01/2024' },
-        { label: 'Phái', value: 'Nữ' },
-        { label: 'Địa chỉ', value: '242 Phạm Văn Đồng, P.Hiệp Bình Chánh, TP.Thủ Đức, TP.HCM' },
-        { label: 'Email', value: 'a.nguyenvnpm@hcmut.edu.vn' },
-        { label: 'Số điện thoại', value: '0123456789' },
-    ])
+  const [clickButton, setClickButton] = useState(false);
     const [lastInfo, setLastInfo] = useState(info)
-    const [clickButton, setClickButton] = useState(false);
+    const changeDatabase = async (newInfo) => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/students/${newInfo._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+          body: JSON.stringify(Object.fromEntries(Object.entries(newInfo).slice(-6))),
+        });
+        if(response.status === 200) {
+          console.log('Database updated successfully');
+        }  else {
+          console.log('Database updated failed');
+          
+        }
+      }
+      catch (error) {
+        console.error(error)
+      }
+    }
     const handleUpdate = () => {
         setClickButton(true);
         setLastInfo(info);
         console.log('Cập nhật: ', info);
+        const newInfo = {
+          _id: info[1].value,
+          email: info[6].value, /// add  // update email in database when change it in this component
+          name: info[0].value,
+          faculty: info[2].value,
+          dob: info[3].value,
+          sex: info[4].value,
+          address: info[5].value,
+          phoneNumber: info[7].value /// add 
+        }
+        console.log('newInfo: ', Object.fromEntries(Object.entries(newInfo).slice(-6)));
+        
+        changeDatabase(newInfo);
     }
 
     const handleCancel = () => {
         setClickButton(true);
         setInfo(lastInfo);
-        console.log('Hủy: ', lastInfo);
+        console.log('Hủy: ', info);
     }
 
     const handlePersonalInfoChange = (newValues: Info[]) => {
@@ -43,6 +147,7 @@ const InfoUser: React.FC = () => {
             updatedInfo[index] = item;
         });
         setInfo(updatedInfo);
+        setClickButton(false);
     };
 
     const handleAccountInfoChange = (newValues: Info[]) => {
@@ -51,8 +156,11 @@ const InfoUser: React.FC = () => {
             updatedInfo[info.length - 2 + index] = item;
         });
         setInfo(updatedInfo);
-    };
+        setClickButton(false);
 
+    };
+  //////////////////// why not change there //////////////////
+  
   return (
     <div>
       <Header title='Thông tin cá nhân'/>
@@ -60,7 +168,7 @@ const InfoUser: React.FC = () => {
       <div className='info-user' style={{marginTop: '100px'}}>
         <InfoFrame 
           title="THÔNG TIN CÁ NHÂN" 
-          info={info.slice(0, -2)} 
+          info={info.slice(0, -2)}
           onValuesChange={handlePersonalInfoChange}
           onResetInput={clickButton}  
         />
