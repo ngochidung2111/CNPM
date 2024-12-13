08b6aec4c6fb5e483193c5b542b9963373c1ebe4
anhkhoaoki <132700613+anhkhoaoki@users.noreply.github.com>
2024-12-13 11:06:15 +0700

    config

--- server/configs/dbconnect.js
+++ server/configs/dbconnect.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const {default: mongoose} = require('mongoose');
 
 const dbConnect = async () => {
@@ -9,33 +8,6 @@ const dbConnect = async () => {
     } catch(error) {
         console.log('DB connection failed');
         throw new Error(error);
-=======
-const { default: mongoose } = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-const dbConnect = async (retries = 3, delay = 5000) => {
-    while (retries > 0) {
-        try {
-            const connect = await mongoose.connect(process.env.MONGODB_URI);
-            if (connect.connection.readyState === 1) {
-                console.log('DB connected successfully');
-                return; // Dừng lại khi kết nối thành công
-            } else {
-                console.log('DB connecting');
-            }
-        } catch (error) {
-            retries -= 1;
-            console.error(`DB connection failed. Retries left: ${retries}`);
-            console.error(error.message);
-
-            if (retries === 0) {
-                throw new Error('All retries failed. DB connection unsuccessful.');
-            }
-
-            // Chờ trước khi thử lại
-            await new Promise((resolve) => setTimeout(resolve, delay));
-        }
->>>>>>> Stashed changes
     }
 }
 
